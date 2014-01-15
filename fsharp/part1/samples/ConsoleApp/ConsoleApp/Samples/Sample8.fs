2681afe644f3f470ea93b6df85806418b589ef7e
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2014-01-15 17:13:22 +0100

    [fsharp] Complete misc features

--- fsharp/part1/samples/ConsoleApp/ConsoleApp/Samples/Sample8.fs
+++ fsharp/part1/samples/ConsoleApp/ConsoleApp/Samples/Sample8.fs
@@ -8,6 +8,22 @@
 module ConsoleApp.Samples.Sample8
 
     module ``Units of measure`` =
+        //open System
         
-        let i = 0
+        [<Measure>] 
+        type F = 
+            static member ToCel(x: float<F>) = (5.0<C> / 9.0<F>) * (x - 32.0<F>)
+        and [<Measure>] C = 
+            static member ToFar(x: float<C>) = (9.0<F> / 5.0<C> * x) + 32.0<F>
+
+        let nowC = 20.0<C>
+        let nowF = C.ToFar(nowC)
+        //let rubbish1 = nowC + nowF
+
+        [<Measure>] 
+        type Sec
+
+        [<Measure>] // Cooling ratio 
+        type CR = C / Sec
+
 
