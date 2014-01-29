13f047da32001a90cc03f2b7adab3b98d470efed
Victor Bartel <victor.bartel@ya.ru>
2014-01-29 21:18:01 +0100

    [fsharp] Updates during presentation

--- fsharp/part1/samples/ConsoleApp/ConsoleApp/Samples/Sample1.fs
+++ fsharp/part1/samples/ConsoleApp/ConsoleApp/Samples/Sample1.fs
@@ -21,11 +21,11 @@ let int16   = 1s   // System.UInt16
 let int64   = 1L   // System.Int64
 let uint64  = 1UL  // System.UInt64
 let double  = 1.0  // System.Double
-let float32  = 1.0f // System.Single
+let float32 = 1.0f // System.Single
 let byte    = 1uy  // System.Byte
 let sbyte   = 1y   // System.SByte
 let decimal = 1M   // System.Decimal
-let bigint  = 1I   // System.Numerics
+let bigint  = 1I   // System.Numerics 
 
 // Arithmetic operations
 
@@ -36,6 +36,7 @@ let op1 = ((float (((1 + 1 * 2) - 1) / 1) ) ** 2.0) % 2.0
 let bop1 = ((((0b1111 &&& 0b0011) ||| 0x00FF) ^^^ 0x1) <<< 1) >>> 1
 
 // Strings
+
 
 let str1 = "111-\
                             222-\
@@ -57,6 +58,7 @@ let _, el2, _ =  tuple1
 let list1 = 0 :: [1; 2; 3]
 let list2 = list1 @ [4 .. 10]
 let list3 = []
+let count  = List.length (list2)
 
 let array1 = [|3 .. 7|]
 
